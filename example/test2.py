fdc93db18878a47eee2dfb7666632fe94dc874ab
Zhenhua Luo <zavier.luo@gmail.com>
2017-02-23 18:21:17 +0800

    example/test2.py: add test code for lib/sql.py
    Signed-off-by: Zhenhua Luo <zavier.luo@gmail.com>

--- example/test2.py
+++ example/test2.py
@@ -5,17 +5,37 @@
 
 import os
 import sys
+import random
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)),
                                 'lib'))
-
-from db import *
+import db
+import sql
 
 def main():
-    db = Mysql()
-    db.connect()
-    db.query()
-    db.disconnect()
+    # test for db.py
+    db1 = db.Mysql()
+    db1.connect()
+    db1.query()
+    db1.disconnect()
+
+    # test for sql.py
+    sql1 = sql.Mysql()
+    res= sql1.query(sql.Testtb1, 'id', 1)
+    print(res)
+
+    names = ("apple", "banana", "orange")
+    name = random.choice(names)
+    print ("the random choice is %s" % name)
+    res.name = name
+    sql1.session.commit()
+    res= sql1.query(sql.Testtb1, 'id', 1)
+    print(res)
+
+    items = sql1.queryall(sql.Testtb1)
+    for res in items:
+        print(res)
+    sql1.queryall();
 
 
 if __name__ == "__main__":
